554aacbe91d0effa0e622e62fa83061a3adb5119
Evans Honu <evanshonu@gmail.com>
2023-05-13 10:50:02 +0000

    Update itm_tranmit.c

--- itm_tranmit.c
+++ itm_tranmit.c
@@ -5,27 +5,24 @@
 //					If you are using Cortex M0, then you can use semihosting feature of openOCD
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-#define DEMCR1                  0xE000EDFCU
-
-#define DEMCR        			*((volatile uint32_t*)DEMCR1)
+#define DEMCR        			*((volatile uint32_t*) 0xE000EDFCU )
 
 /* ITM register addresses */
 #define ITM_STIMULUS_PORT0   	*((volatile uint32_t*) 0xE0000000 )
 #define ITM_TRACE_EN          	*((volatile uint32_t*) 0xE0000E00 )
 
-void ITM_SendChar(uint8_t ch)
+void ITM_SendChar(uint16_t character)
 {
 
 	//Enable TRCENA
-	DEMCR |= ( 1 << 24);
+	DEMCR |= 0x1000000;
 
 	//enable stimulus port 0
-	ITM_TRACE_EN |= ( 1 << 0);
+	ITM_TRACE_EN |= 0x1;
 
 	// read FIFO status in bit [0]:
 	while(!(ITM_STIMULUS_PORT0 & 1));
 
 	//Write to ITM stimulus port0
-	ITM_STIMULUS_PORT0 = ch;
+	ITM_STIMULUS_PORT0 = character;
 }
