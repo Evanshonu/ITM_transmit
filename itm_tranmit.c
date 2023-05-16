d199421aea7e655e9a249ae7f24ba595be6ec877
Evans Honu <evanshonu@gmail.com>
2023-05-16 17:42:06 +0000

    Update itm_tranmit.c

--- itm_tranmit.c
+++ itm_tranmit.c
@@ -1,10 +1,22 @@
+/*
+ITM (Instrumentation Trace Macrocell) is a powerful feature available in STM32 microcontrollers, 
+designed to aid in debugging and performance analysis of embedded systems. 
+It provides a means of capturing and tracing real-time program execution data, 
+such as CPU cycles, instruction execution, and application-specific events.
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-//					Implementation of printf like feature using ARM Cortex M3/M4/ ITM functionality
-//					This function will not work for ARM Cortex M0/M0+
-//					If you are using Cortex M0, then you can use semihosting feature of openOCD
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+The ITM module integrates with the ARM Cortex-M core found in STM32 microcontrollers,offering advanced debugging capabilities. 
+ It allows developers to instrument their code by inserting trace points at specific locations, 
+ which can be triggered to record relevant data during program execution. These trace points can be 
+ placed at various levels, ranging from individual instructions to function calls or custom events.
 
+
+
+
+
+
+
+
+*/
 #define DEMCR        			*((volatile uint32_t*) 0xE000EDFCU )
 
 /* ITM register addresses */
